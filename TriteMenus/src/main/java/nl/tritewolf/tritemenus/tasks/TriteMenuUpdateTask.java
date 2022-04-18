2403a7f4d11db65a1cef6890e3426272716722de
Itz_KiwiSap_ <programmingjesse@gmail.com>
2022-04-18 15:23:31 +0200

    Added injection for processor in update task

--- TriteMenus/src/main/java/nl/tritewolf/tritemenus/tasks/TriteMenuUpdateTask.java
+++ TriteMenus/src/main/java/nl/tritewolf/tritemenus/tasks/TriteMenuUpdateTask.java
@@ -1,8 +1,13 @@
 package nl.tritewolf.tritemenus.tasks;
 
+import nl.tritewolf.tritejection.annotations.TriteJect;
+import nl.tritewolf.tritemenus.menu.TriteMenuProcessor;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class TriteMenuUpdateTask extends BukkitRunnable {
+
+    @TriteJect
+    private TriteMenuProcessor triteMenuProcessor;
 
     @Override
     public void run() {
