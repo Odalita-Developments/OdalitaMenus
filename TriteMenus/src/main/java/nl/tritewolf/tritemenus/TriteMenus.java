d58b58632a0b69caf2f36640f4e5113c8383874e
Itz_KiwiSap_ <programmingjesse@gmail.com>
2022-04-18 20:57:13 +0200

    Update items on different thread

--- TriteMenus/src/main/java/nl/tritewolf/tritemenus/TriteMenus.java
+++ TriteMenus/src/main/java/nl/tritewolf/tritemenus/TriteMenus.java
@@ -10,6 +10,8 @@ import nl.tritewolf.tritemenus.tasks.TriteMenuUpdateTask;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Getter
 public class TriteMenus {
@@ -35,6 +37,6 @@ public class TriteMenus {
             e.printStackTrace();
         }
 
-        triteMenus.getTriteJection(TriteMenuUpdateTask.class).runTaskTimer(javaPlugin, 0, 1);
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(triteMenus.getTriteJection(TriteMenuUpdateTask.class), 0, 50, TimeUnit.MILLISECONDS);
     }
 }
--- TriteMenus/src/main/java/nl/tritewolf/tritemenus/tasks/TriteMenuUpdateTask.java
+++ TriteMenus/src/main/java/nl/tritewolf/tritemenus/tasks/TriteMenuUpdateTask.java
@@ -6,45 +6,57 @@ import net.minecraft.server.v1_16_R3.PacketPlayOutSetSlot;
 import nl.tritewolf.tritejection.annotations.TriteJect;
 import nl.tritewolf.tritemenus.contents.TriteSlotPos;
 import nl.tritewolf.tritemenus.items.TriteMenuItem;
+import nl.tritewolf.tritemenus.items.TriteUpdatableItem;
 import nl.tritewolf.tritemenus.menu.TriteMenuObject;
 import nl.tritewolf.tritemenus.menu.TriteMenuProcessor;
 import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_16_R3.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.Collection;
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class TriteMenuUpdateTask extends BukkitRunnable {
+public class TriteMenuUpdateTask implements Runnable {
 
     @TriteJect
     private TriteMenuProcessor triteMenuProcessor;
 
+    private static final AtomicInteger TICKS = new AtomicInteger(0);
+
     @Override
     public void run() {
+        int ticks = TICKS.incrementAndGet();
+
         for (Map.Entry<UUID, Map<Class<?>, TriteMenuObject>> menus : triteMenuProcessor.getMenus().entrySet()) {
             TriteMenuObject triteMenuObject = menus.getValue().values().stream().filter(menuObject -> menuObject.isHasUpdatableItems() && menuObject.isHasMenuOpened()).findFirst().orElse(null);
-            if (triteMenuObject != null) {
-                for (Map.Entry<TriteSlotPos, TriteMenuItem> menuItemEntry : triteMenuObject.getContents().entrySet()) {
+            Player player = Bukkit.getPlayer(menus.getKey());
 
-                    triteMenuObject.getInventory().setItem(menuItemEntry.getKey().getSlot(), menuItemEntry.getValue().getItemStack());
+            if (triteMenuObject != null && player != null && player.isOnline()) {
+                for (Map.Entry<TriteSlotPos, TriteMenuItem> menuItemEntry : triteMenuObject.getContents().entrySet()) {
+                    if (menuItemEntry.getValue() instanceof TriteUpdatableItem) {
+                        TriteUpdatableItem updatableItem = (TriteUpdatableItem) menuItemEntry.getValue();
+
+                        if (ticks % updatableItem.getUpdateTicks() == 0) {
+                            int slot = menuItemEntry.getKey().getSlot();
+                            org.bukkit.inventory.ItemStack item = menuItemEntry.getValue().getItemStack();
+
+                            this.updateItem(player, slot, item);
+                        }
+                    }
                 }
             }
         }
-
     }
 
-    public static void updateItem(Player player, int slot, org.bukkit.inventory.ItemStack itemStack) {
+    private void updateItem(Player player, int slot, org.bukkit.inventory.ItemStack itemStack) {
         EntityPlayer handle = ((CraftPlayer) player).getHandle();
+        int windowId = handle.activeContainer.windowId;
 
         ItemStack copy = CraftItemStack.asNMSCopy(itemStack);
 
-        PacketPlayOutSetSlot pack = new PacketPlayOutSetSlot(-1, slot, copy);
+        PacketPlayOutSetSlot pack = new PacketPlayOutSetSlot(windowId, slot, copy);
         handle.playerConnection.sendPacket(pack);
     }
 }
