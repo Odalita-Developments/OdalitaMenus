b4fd0ff3ac4ff25621913f194791466d3fc0e383
TriteWolf <r.vanleeuwerden@outlook.com>
2022-04-21 17:47:37 +0200

    Fixed buggy bottom inventory when inventory had placeableslots.
    Took 55 minutes

--- src/main/java/nl/tritewolf/tritemenus/listeners/InventoryListener.java
+++ src/main/java/nl/tritewolf/tritemenus/listeners/InventoryListener.java
@@ -5,22 +5,15 @@ import nl.tritewolf.tritemenus.contents.SlotPos;
 import nl.tritewolf.tritemenus.items.MenuItem;
 import nl.tritewolf.tritemenus.menu.MenuObject;
 import nl.tritewolf.tritemenus.menu.MenuProcessor;
-import org.bukkit.Material;
-import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
-import javax.swing.*;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -40,14 +33,12 @@ public final class InventoryListener implements Listener {
 
         if (event.getView().getTopInventory().equals(openMenuObject.getInventory())) {
             List<Integer> placeableItems = openMenuObject.getPlaceableItems();
-            if (!placeableItems.isEmpty() && event.getView().getBottomInventory().equals(clickedInventory))
-                return;
-
-            if (event.getClick().isShiftClick() && clickedInventory != null && clickedInventory.equals(event.getView().getTopInventory())) {
+            if (event.getClick().isShiftClick() && !event.getView().getTopInventory().equals(clickedInventory)) {
                 event.setCancelled(true);
                 return;
             }
 
+            if (!placeableItems.isEmpty() && event.getView().getBottomInventory().equals(clickedInventory)) return;
             if (placeableItems.contains(event.getSlot())) return;
 
             event.setCancelled(true);
@@ -63,11 +54,15 @@ public final class InventoryListener implements Listener {
     public void onInventoryDrag(InventoryDragEvent event) {
         Player player = (Player) event.getWhoClicked();
         MenuObject openMenuObject = this.menuProcessor.getOpenMenus().get(player.getUniqueId());
-        if (event.getView().getTopInventory().equals(openMenuObject.getInventory())) {
-            List<Integer> placeableItems = openMenuObject.getPlaceableItems();
-            Set<Integer> inventorySlots = event.getInventorySlots();
-            boolean matchAllSlots = new HashSet<>(placeableItems).containsAll(inventorySlots);
+        List<Integer> placeableItems = openMenuObject.getPlaceableItems();
 
+        if (event.getView().getTopInventory().equals(openMenuObject.getInventory())) {
+            Set<Integer> inventorySlots = event.getInventorySlots();
+
+            boolean b = event.getRawSlots().stream().allMatch(integer -> integer > (openMenuObject.getInventory().getSize() - 1));
+            if (!placeableItems.isEmpty() && b) return;
+
+            boolean matchAllSlots = new HashSet<>(placeableItems).containsAll(inventorySlots);
             if (!matchAllSlots) {
                 event.setCancelled(true);
             }
