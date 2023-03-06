816716be5956dda5b9cda28cf1f49d25f6ac5dbb
Itz_KiwiSap_ <programmingjesse@gmail.com>
2023-03-06 11:58:09 +0100

    Small cleanup

--- core/src/main/java/nl/odalitadevelopments/menus/utils/version/ProtocolVersion.java
+++ core/src/main/java/nl/odalitadevelopments/menus/utils/version/ProtocolVersion.java
@@ -3,9 +3,6 @@ package nl.odalitadevelopments.menus.utils.version;
 import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public enum ProtocolVersion {
 
     MINECRAFT_1_19_3(761),
@@ -50,44 +47,14 @@ public enum ProtocolVersion {
         return this.number == version.getNumber();
     }
 
-    private static final String SERVER_VERSION_STRING;
     private static final ProtocolVersion SERVER_VERSION;
-    private static final Map<Integer, ProtocolVersion> PROTOCOL_VERSIONS = new LinkedHashMap<>();
 
     static {
-        SERVER_VERSION_STRING = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
         SERVER_VERSION = getByVersionString(Bukkit.getServer().getVersion().split("\\(MC: ")[1].split("\\)")[0]);
-
-        for (ProtocolVersion version : values()) {
-            PROTOCOL_VERSIONS.put(version.number, version);
-        }
-    }
-
-    public static @NotNull String getServerVersionString() {
-        return SERVER_VERSION_STRING;
     }
 
     public static @NotNull ProtocolVersion getServerVersion() {
         return SERVER_VERSION;
-    }
-
-    public static @NotNull ProtocolVersion getVersion(int versionNumber) {
-        ProtocolVersion protocolVersion = PROTOCOL_VERSIONS.get(versionNumber);
-        return (protocolVersion == null) ? NOT_SUPPORTED : protocolVersion;
-    }
-
-    public static @NotNull ProtocolVersion matchVersion(int versionNumber) {
-        ProtocolVersion protocolVersion = getVersion(versionNumber);
-
-        if (protocolVersion == NOT_SUPPORTED) {
-            for (ProtocolVersion version : values()) {
-                if (version.getNumber() > versionNumber) continue;
-
-                return version;
-            }
-        }
-
-        return protocolVersion;
     }
 
     public static @NotNull ProtocolVersion getByVersionString(@NotNull String versionString) {
