8cc44fb3fef02abee1489934ca693966a0b05e6c
Itz_KiwiSap_ <programmingjesse@gmail.com>
2023-12-06 13:17:23 +0100

    Made Identity abstract class

--- core/src/main/java/nl/odalitadevelopments/menus/identity/Identity.java
+++ core/src/main/java/nl/odalitadevelopments/menus/identity/Identity.java
@@ -2,14 +2,36 @@ package nl.odalitadevelopments.menus.identity;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.ParameterizedType;
+import java.util.Objects;
 
-public interface Identity<T> {
+public abstract class Identity<T> {
 
-    @NotNull T ident();
+    private final T ident;
+
+    public Identity(@NotNull T ident) {
+        this.ident = ident;
+    }
+
+    public final @NotNull T ident() {
+        return this.ident;
+    }
 
     @SuppressWarnings("unchecked")
-    default @NotNull Class<T> type() {
-        return (Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+    public final @NotNull Class<T> type() {
+        return (Class<T>) this.ident.getClass();
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Identity<?> identity = (Identity<?>) o;
+        return Objects.equals(this.ident, identity.ident);
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(this.ident);
     }
 }
