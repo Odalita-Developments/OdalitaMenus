28f992c62aaf3a7951315114cd290170a8ef5cc5
Tom <32574425+ItzCodex@users.noreply.github.com>
2025-06-14 11:06:24 +0200

    Added support for Minecraft version 1.21.5

---
+++ nms/v1_21_R4/src/main/java/nl/odalitadevelopments/menus/nms/v1_21_R4/ObfuscatedNames_v1_21_R4.java
@@ -0,0 +1,15 @@
+package nl.odalitadevelopments.menus.nms.v1_21_R4;
+
+public final class ObfuscatedNames_v1_21_R4 {
+
+    private ObfuscatedNames_v1_21_R4() {
+    }
+
+    public static final String NETWORK_MANAGER = "connection";
+    public static final String WINDOW_ID = "containerId";
+
+    public static final String SET_CONTENTS_PACKET_ITEMS = "items";
+    public static final String SET_CONTENTS_PACKET_CARRIED_ITEM = "carriedItem";
+
+    public static final String SET_SLOT_PACKET_ITEM = "itemStack";
+}
