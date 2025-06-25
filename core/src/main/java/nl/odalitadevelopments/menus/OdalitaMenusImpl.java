2e5cc5e529f7ef16c4b9e88ca09c1c6148974153
Tom <32574425+ItzCodex@users.noreply.github.com>
2025-06-25 18:37:31 +0200

    Also added 1_21_R5

--- core/src/main/java/nl/odalitadevelopments/menus/OdalitaMenusImpl.java
+++ core/src/main/java/nl/odalitadevelopments/menus/OdalitaMenusImpl.java
@@ -30,6 +30,7 @@ import nl.odalitadevelopments.menus.nms.v1_20_R2.OdalitaMenusNMS_v1_20_R2;
 import nl.odalitadevelopments.menus.nms.v1_21_R2.OdalitaMenusNMS_v1_21_R2;
 import nl.odalitadevelopments.menus.nms.v1_21_R3.OdalitaMenusNMS_v1_21_R3;
 import nl.odalitadevelopments.menus.nms.v1_21_R4.OdalitaMenusNMS_v1_21_R4;
+import nl.odalitadevelopments.menus.nms.v1_21_R5.OdalitaMenusNMS_v1_21_R5;
 import nl.odalitadevelopments.menus.patterns.MenuPattern;
 import nl.odalitadevelopments.menus.patterns.PatternContainer;
 import nl.odalitadevelopments.menus.providers.ProvidersContainer;
@@ -141,6 +142,7 @@ final class OdalitaMenusImpl implements OdalitaMenus, Listener {
         try {
             Class<?> harmNMSInstance = Class.forName("nl.odalitadevelopments.menus.nms.OdalitaMenusNMSInstance");
             OdalitaMenusNMS nms = switch (serverVersion) {
+                case MINECRAFT_1_21_6 -> new OdalitaMenusNMS_v1_21_R5();
                 case MINECRAFT_1_21_5 -> new OdalitaMenusNMS_v1_21_R4();
                 case MINECRAFT_1_21_4 -> new OdalitaMenusNMS_v1_21_R3();
                 case MINECRAFT_1_21_3 -> new OdalitaMenusNMS_v1_21_R2();
